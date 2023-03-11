7a47523504e7e9f93ec8b30b26d1093b2714b362
Tess Sinamal <tess.sinamal@gmail.com>
2023-03-11 20:05:37 +0100

    feat(vision): turn dark mode on

--- tensorflow-examples/src/App.tsx
+++ tensorflow-examples/src/App.tsx
@@ -1,44 +1,22 @@
-import React, {useState} from "react";
+import React from "react";
 import "./App.css";
-import { useData } from "./vision/cars/http/useData";
-import { getModel, useTrainModel } from "./vision/cars/model";
-import { ScatterChart } from "./vision/cars/components/ScatterChart";
-import { dataToChartData } from "./vision/cars/adapters";
-import { Dashboard } from "./vision/cars/components/Dashboard";
-import { ModelSummary } from "./vision/cars/components/Table";
-import { LineChart } from "./vision/cars/components/LineChart";
-
-interface TrainingData {
-  loss: number,
-  mse: number,
-  epoch: number
-}
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Home } from "./vision/cars/views/Home";
 
 function App() {
-  const [data] = useData();
-  const [dataTraining, setDataTraining] = useState<TrainingData[]>([]);
-  const chartData = dataToChartData(data);
-  const options = {
-    yLabel: "mpg",
-    xLabel: "hp",
-    name: "Horsepower vs MPG",
-  };
-  const model = getModel();
-
-  const handleNewTrainingData = (trainingData: TrainingData) => {
-    setDataTraining(prevState => [...prevState, trainingData])
-  }
-
-  useTrainModel(model, data, handleNewTrainingData);
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
 
   return (
-    <div className="App">
-      <Dashboard>
-        <ScatterChart data={chartData} options={options} />
-        <LineChart data={dataTraining} options={options} />
-        <ModelSummary model={model} />
-      </Dashboard>
-    </div>
+    <>
+      {/* @ts-ignore */}
+      <ThemeProvider theme={darkTheme}>
+        <Home />
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ tensorflow-examples/src/vision/cars/views/Home.tsx
@@ -0,0 +1,42 @@
+import { Dashboard } from "../components/Dashboard";
+import { ScatterChart } from "../components/ScatterChart";
+import { LineChart } from "../components/LineChart";
+import { ModelSummary } from "../components/Table";
+import React, { useState } from "react";
+import { useData } from "../http/useData";
+import { dataToChartData } from "../adapters";
+import { getModel, useTrainModel } from "../model";
+
+interface TrainingData {
+  loss: number;
+  mse: number;
+  epoch: number;
+}
+
+export const Home = () => {
+  const [data] = useData();
+  const [dataTraining, setDataTraining] = useState<TrainingData[]>([]);
+  const chartData = dataToChartData(data);
+  const options = {
+    yLabel: "mpg",
+    xLabel: "hp",
+    name: "Horsepower vs MPG",
+  };
+  const model = getModel();
+
+  const handleNewTrainingData = (trainingData: TrainingData) => {
+    setDataTraining((prevState) => [...prevState, trainingData]);
+  };
+
+  useTrainModel(model, data, handleNewTrainingData);
+
+  return (
+    <div className="App">
+      <Dashboard>
+        <ScatterChart data={chartData} options={options} />
+        <LineChart data={dataTraining} options={options} />
+        <ModelSummary model={model} />
+      </Dashboard>
+    </div>
+  );
+};
