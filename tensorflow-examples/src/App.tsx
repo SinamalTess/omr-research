cd8dd2a83408a0ffa55434f94aa7f475b79420bc
Tess Sinamal <tess.sinamal@gmail.com>
2023-03-11 03:19:24 +0100

    chore(config): configure prettier and run formatting

--- tensorflow-examples/src/App.tsx
+++ tensorflow-examples/src/App.tsx
@@ -1,21 +1,18 @@
-import React, {useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {getData} from "./vision/cars/http";
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getData } from "./vision/cars/http";
 
 function App() {
-
-  useEffect (() => {
-    getData().then(data => console.log(data))
-  }, [])
+  useEffect(() => {
+    getData().then((data) => console.log(data));
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
+        <p>Hello World!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
