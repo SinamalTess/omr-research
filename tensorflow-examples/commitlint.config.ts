e9be25b18ce0c8417213af892bc54fd946c343ff
Tess Sinamal <tess.sinamal@gmail.com>
2023-03-11 02:49:29 +0100

    chore(config): replace cz-conventional-changelog by commitiquette, commitizen, commitlint

---
+++ tensorflow-examples/commitlint.config.ts
@@ -0,0 +1,10 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': [2, 'always', ['docs', 'vision', 'config', 'other']]
+    }
+};
+
+module.exports = Configuration;
