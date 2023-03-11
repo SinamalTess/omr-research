ad178512db6f33bd71f9aa49c940c43fee97dcf4
Tess Sinamal <tess.sinamal@gmail.com>
2023-03-11 16:59:04 +0100

    fix(vision): define properly Car interface

--- tensorflow-examples/src/vision/cars/domain/car.entity.ts
+++ tensorflow-examples/src/vision/cars/domain/car.entity.ts
@@ -1,13 +1,13 @@
 export interface Car {
-  Name: "chevrolet chevelle malibu";
-  Miles_per_Gallon: 18;
-  Cylinders: 8;
-  Displacement: 307;
-  Horsepower: 130;
-  Weight_in_lbs: 3504;
-  Acceleration: 12;
-  Year: "1970-01-01";
-  Origin: "USA";
+  Name: string;
+  Miles_per_Gallon: number;
+  Cylinders: number;
+  Displacement: number;
+  Horsepower: number;
+  Weight_in_lbs: number;
+  Acceleration: number;
+  Year: string;
+  Origin: string;
 }
 
 export type NormalizedCar = Omit<Car, "Miles_per_Gallon" | "Horsepower"> & {
