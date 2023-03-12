3643403ca21b1ef4dc943cb80845ac388c5caac0
Tess Sinamal <tess.sinamal@gmail.com>
2023-03-12 15:22:59 +0100

    feat(vision): introduce new data shape tab

--- tensorflow-examples/src/App.tsx
+++ tensorflow-examples/src/App.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Home } from "./vision/cars/views/Home";
-import { styled } from "@mui/material";
+import { Box, styled, Tab, Typography } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Navbar } from "./vision/cars/components/Dashboard/Navbar";
+import { Controls } from "./vision/cars/components";
+import { useData } from "./vision/cars/http";
+import { TrainingData } from "./vision/types";
+import { Coordinates } from "./vision/cars/domain";
+import { getModel, startTraining } from "./vision/cars/model";
+import { dataToCoordinates } from "./vision/cars/adapters";
 
 const StyledHome = styled(Home)`
   padding: ${({ theme }) => theme.spacing(2)};
 `;
+
+type TrainingStatus = "waiting" | "training" | "done";
 
 function App() {
   const darkTheme = createTheme({
@@ -14,9 +24,88 @@ function App() {
     },
   });
 
+  const [data] = useData();
+  const [epochs, setEpochs] = useState(50);
+  const [currentEpoch, setCurrentEpoch] = useState(0);
+  const [dataTraining, setDataTraining] = useState<TrainingData[]>([]);
+  const [predictions, setPredictions] = useState<Coordinates[]>([]);
+  const [model, setModel] = useState(getModel());
+  const [activeTab, setActiveTab] = useState("1");
+  const [trainingStatus, setTrainingStatus] =
+    useState<TrainingStatus>("waiting");
+  const chartData = dataToCoordinates(data);
+
+  const reset = () => {
+    setCurrentEpoch(0);
+    setModel(getModel());
+    setDataTraining([]);
+    setPredictions([]);
+  };
+
+  const handleEpochEnd = (trainingData: TrainingData) => {
+    const { epoch } = trainingData;
+    setDataTraining((prevState) => [...prevState, trainingData]);
+    setCurrentEpoch(epoch);
+  };
+
+  const handleTrainingEnd = (predictions: Coordinates[]) => {
+    setPredictions(predictions);
+    setTrainingStatus("done");
+  };
+
+  const train = () => {
+    reset();
+    setTrainingStatus("training");
+    startTraining(model, data, {
+      epochs,
+      onEpochEnd: handleEpochEnd,
+      onTrainingEnd: handleTrainingEnd,
+    });
+  };
+
+  const handleEpochsChanged = (newEpochsValue: number) => {
+    reset();
+    setEpochs(newEpochsValue);
+  };
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
+    setActiveTab(newValue);
+  };
+
+  const isTraining = trainingStatus === "training";
+
   return (
     <ThemeProvider theme={darkTheme}>
-      <StyledHome />
+      <Navbar>
+        <Typography variant="h4" component="h1" color={"primary"}>
+          Horsepower vs MPG (miles per gallon)
+        </Typography>
+        <Controls
+          epochs={epochs}
+          currentEpoch={currentEpoch}
+          isTraining={isTraining}
+          onClick={train}
+          onChangeEpochs={handleEpochsChanged}
+          onEnterKeyDown={train}
+        />
+      </Navbar>
+      <TabContext value={activeTab}>
+        <Box>
+          <TabList onChange={handleTabChange}>
+            <Tab label="Home" value="1" />
+            <Tab label="Data Shape" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          <StyledHome
+            model={model}
+            predictions={predictions}
+            dataPreview={chartData}
+            dataTraining={dataTraining}
+          />
+        </TabPanel>
+        <TabPanel value="1"><Typography>test</Typography></TabPanel>
+      </TabContext>
     </ThemeProvider>
   );
 }
--- tensorflow-examples/src/vision/cars/views/Home.tsx
+++ tensorflow-examples/src/vision/cars/views/Home.tsx
@@ -1,93 +1,39 @@
-import { ModelSummary, Controls } from "../components";
-import React, { useState } from "react";
-import { useData } from "../http";
-import { dataToCoordinates } from "../adapters";
-import { getModel, startTraining } from "../model";
+import { ModelSummary } from "../components";
+import React from "react";
 import { DataPreview } from "./DataPreview";
 import { TrainingPreview } from "./TrainingPreview";
-import { TrainingData } from "../../types";
-import { Typography } from "@mui/material";
-import { Coordinates } from "../domain";
-import { Navbar } from "../components/Dashboard/Navbar";
 import { MainPreview } from "../components/Dashboard/MainPreview";
 import { Sidebar } from "../components/Dashboard/Sidebar";
 import { Dashboard } from "../components";
+import { Coordinates } from "../domain";
+import { TrainingData } from "../../types";
+import * as tf from "@tensorflow/tfjs";
+
+interface HomeProps {
+  className?: string;
+  dataPreview: Coordinates[];
+  predictions: Coordinates[];
+  model: tf.LayersModel;
+  dataTraining: TrainingData[];
+}
 
 const options = {
   yLabel: "mpg",
   xLabel: "hp",
 };
 
-type TrainingStatus = "waiting" | "training" | "done";
-
-interface HomeProps {
-  className?: string;
-}
-
-export const Home = ({ className }: HomeProps) => {
-  const [data] = useData();
-  const [epochs, setEpochs] = useState(50);
-  const [currentEpoch, setCurrentEpoch] = useState(0);
-  const [dataTraining, setDataTraining] = useState<TrainingData[]>([]);
-  const [predictions, setPredictions] = useState<Coordinates[]>([]);
-  const [model, setModel] = useState(getModel());
-  const [trainingStatus, setTrainingStatus] =
-    useState<TrainingStatus>("waiting");
-  const chartData = dataToCoordinates(data);
-
-  const reset = () => {
-    setCurrentEpoch(0);
-    setModel(getModel());
-    setDataTraining([]);
-    setPredictions([]);
-  };
-
-  const handleEpochEnd = (trainingData: TrainingData) => {
-    const { epoch } = trainingData;
-    setDataTraining((prevState) => [...prevState, trainingData]);
-    setCurrentEpoch(epoch);
-  };
-
-  const handleTrainingEnd = (predictions: Coordinates[]) => {
-    setPredictions(predictions);
-    setTrainingStatus("done");
-  };
-
-  const train = () => {
-    reset();
-    setTrainingStatus("training");
-    startTraining(model, data, {
-      epochs,
-      onEpochEnd: handleEpochEnd,
-      onTrainingEnd: handleTrainingEnd,
-    });
-  };
-
-  const handleEpochsChanged = (newEpochsValue: number) => {
-    reset();
-    setEpochs(newEpochsValue);
-  };
-
-  const isTraining = trainingStatus === "training";
-
+export const Home = ({
+  className,
+  dataPreview,
+  predictions,
+  dataTraining,
+  model,
+}: HomeProps) => {
   return (
     <Dashboard className={className}>
-      <Navbar>
-        <Typography variant="h4" component="h1" color={"primary"}>
-          Horsepower vs MPG (miles per gallon)
-        </Typography>
-        <Controls
-          epochs={epochs}
-          currentEpoch={currentEpoch}
-          isTraining={isTraining}
-          onClick={train}
-          onChangeEpochs={handleEpochsChanged}
-          onEnterKeyDown={train}
-        />
-      </Navbar>
       <MainPreview>
         <DataPreview
-          data={chartData}
+          data={dataPreview}
           options={options}
           predictions={predictions}
         />
