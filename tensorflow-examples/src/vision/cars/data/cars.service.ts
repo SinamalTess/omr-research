aac3bfbdd7f5646cc771f1fd9042f1aa2313cd38
Tess Sinamal <tess.sinamal@gmail.com>
2023-03-12 17:50:38 +0100

    refactor(vision): remove car service

--- tensorflow-examples/src/vision/cars/data/cars.service.ts
+++
@@ -1,8 +0,0 @@
-import { Car } from "../domain";
-import { fetchData } from "../http";
-
-export const getCarsData = async (): Promise<Car[]> => {
-  const url = "https://storage.googleapis.com/tfjs-tutorials/carsData.json";
-
-  return await fetchData<Car[]>(url);
-};
